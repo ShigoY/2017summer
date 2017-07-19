f194712b8cf7bd1799528a5d9330d6452abbd1ca
shigo <l_coder@sina.cn>
2017-07-19 17:58:13 +0200

    leisure exercise

---
+++ OnlineJudge/usaco/stage1_3/barn1.java
@@ -0,0 +1,17 @@
+/*
+ID: l_coder1
+LANG: JAVA
+TASK: barn1
+ */
+
+//package stage1_3;
+import java.io.*;
+import java.util.*;
+
+class barn1 {
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+
+	}
+
+}
